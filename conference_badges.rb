e9a67e6b719eea580bb55a7a951fbfec9fadb268
Tharon Weighill <tharonweighill@gmail.com>
2020-07-31 21:37:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badge_messages = []
+  attendees.each do |name|
+  message = badge_maker(name)
+  badge_messages.push(message) 
+      end
+  return badge_messages
+end
+
+
+def assign_rooms(rooms)
+  
