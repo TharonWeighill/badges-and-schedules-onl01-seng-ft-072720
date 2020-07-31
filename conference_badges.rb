5dbfd3bc9fda4489f0657b98072b20035fb9fa67
Tharon Weighill <tharonweighill@gmail.com>
2020-07-31 21:41:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,28 +1,6 @@
-
-
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
-
-def batch_badge_creator(attendees)
-  badge_messages = []
-  attendees.each do |name|
-  message = badge_maker(name)
-  badge_messages.push(message) 
-      end
-  return badge_messages
-end
-
-
-def assign_rooms(rooms)
-  
-  
-  
-def badge_maker(name)
-  return "Hello, my name is #{name}."
-end
-
-
 
 def batch_badge_creator(array)
   nuarray = []
@@ -31,10 +9,6 @@ def batch_badge_creator(array)
   end
   return nuarray
 end
-
-# batch_badge_creator(people)
-
-
 
 def assign_rooms(array)
   nuarray = []
@@ -45,8 +19,6 @@ def assign_rooms(array)
   end
   return nuarray
 end
-
-# assign_rooms(people)
 
 def printer(array)
   batch_badge_creator(array).each do |id|
